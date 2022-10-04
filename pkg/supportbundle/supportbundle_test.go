97a1e21348e5cb33d61fa5cd3553da8c532710af
ada mancini <ada@replicated.com>
2022-10-04 18:53:20 +0000

    moved from #702

--- pkg/supportbundle/supportbundle_test.go
+++ pkg/supportbundle/supportbundle_test.go
@@ -3,6 +3,11 @@ package supportbundle
 import (
 	"reflect"
 	"testing"
+
+	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Test_LoadAndConcatSpec(t *testing.T) {
@@ -43,4 +48,111 @@ func Test_LoadAndConcatSpec(t *testing.T) {
 		t.Error("Full bundle and concatenated bundle are not the same.")
 	}
 
+}
+
+func Test_ParseSupportBundle(t *testing.T) {
+
+	upstreamSupportBundleDoc := []byte(`
+apiVersion: troubleshoot/v1beta2
+kind: SupportBundle
+spec:
+  uri: https://go-test/upstream.yaml
+	collectors:
+	  cluster-info: {}
+		cluster-resources: {}
+	analyzers: {}`)
+
+	tests := []struct {
+		name    string
+		given   []byte
+		expect  *troubleshootv1beta2.SupportBundle
+		wantErr bool
+	}{
+		{
+			name: "given a spec with a uri should return the upstream spec",
+			given: []byte(`
+apiVersion: troubleshoot/v1beta2
+kind: SupportBundle
+spec:
+  uri: https://go-test/upstream.yaml
+	collectors:
+	  cluster-info: {}
+	analyzers: {}`),
+			expect: &troubleshootv1beta2.SupportBundle{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "troubleshoot/v1beta2",
+					Kind:       "SupportBundle",
+				},
+				Spec: troubleshootv1beta2.SupportBundleSpec{
+					Uri: "https://go-test/upstream.yaml",
+					Collectors: []*troubleshootv1beta2.Collect{
+						{
+							ClusterInfo: &troubleshootv1beta2.ClusterInfo{},
+						},
+						{
+							ClusterResources: &troubleshootv1beta2.ClusterResources{},
+						},
+					},
+					Analyzers: []*troubleshootv1beta2.Analyze{},
+				},
+			},
+			wantErr: false,
+		},
+		// 		{
+		// 			name: "if the uri is unreachable, return the given spec",
+		// 			upstream: []byte(`
+		// apiVersion: troubleshoot/v1beta2
+		// kind: SupportBundle
+		// spec:
+		//   uri: https://go-test/upstream.yaml
+		// 	collectors:
+		// 	  cluster-info: {}
+		// 		cluster-resources: {}
+		// 	analyzers: {}`),
+		// 			given:   &troubleshootv1beta2.SupportBundle{},
+		// 			expect:  given,
+		// 			wantErr: false,
+		// 		},
+		// 		{
+		// 			name: "if the upstream spec is unparseable, return the given spec",
+		// 			upstream: []byte(`
+		// apiVersion: troubleshoot/v1beta2
+		// kind: SupportBundle
+		// spec:
+		//   uri: https://go-test/upstream.yaml
+		// 	collectors:
+		// 	  cluster-info: {}
+		// 		cluster-resources: {}
+		// 	analyzers: {}`),
+		// 			given:   &troubleshootv1beta2.SupportBundle{},
+		// 			expect:  given,
+		// 			wantErr: false,
+		// 		},
+		// 		{
+		// 			name: "if the given spec is only a uri, and if it is unreachable, return an error",
+		// 			upstream: []byte(`
+		// apiVersion: troubleshoot/v1beta2
+		// kind: SupportBundle
+		// spec:
+		//   uri: https://go-test/upstream.yaml
+		// 	collectors:
+		// 	  cluster-info: {}
+		// 		cluster-resources: {}
+		// 	analyzers: {}`),
+		// 			given:   &troubleshootv1beta2.SupportBundle{},
+		// 			expect:  nil,
+		// 			wantErr: true,
+		// 		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := require.New(t)
+
+			givenBundle, err := ParseSupportBundle(test.given, true)
+			req.NoError(err)
+
+			assert.Equal(t, test.expect, givenBundle)
+		})
+	}
 }
